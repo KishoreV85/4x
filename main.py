a7991335a1af0e9b8bc227631a5e1efcc0a87cfc
kishoreV85 <kishore.v@yantravision.com>
2025-02-11 17:25:00 +0530

    2X

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from RealESRGAN import RealESRGAN
 
 def main() -> int:
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = RealESRGAN(device, scale=4)
-    model.load_weights(4)
+    model = RealESRGAN(device, scale=2)
+    model.load_weights(2)
     for i, image in enumerate([f for f in os.listdir("inputs") if f.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp'))]):
         image = Image.open(f"inputs/{image}").convert('RGB')
         sr_image = model.predict(image)
